323d5111b8ceaa0ce42d035d18e8b0e06eecacbe
abhishekgautam2014 <abhishekgautam76@gmail.com>
2025-10-12 13:55:08 +0530

    added percentage change

--- src/queries/bigqueryQueries.js
+++ src/queries/bigqueryQueries.js
@@ -1,31 +1,72 @@
 // This file contains all the BigQuery queries used in the application.
 
 const accountSummaryMetrices = `
-SELECT 
-  -- Core spend & sales metrics
-  IFNULL(SUM(ad_spend), 0) AS ad_spend,
-  IFNULL(SUM(ad_revenue), 0) AS ad_revenue,
-  IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0) AS total_sales,
+WITH
+-- Calculate dynamic previous period based on date range
+date_ranges AS (
+  SELECT 
+    DATE(@startDate) AS start_date,
+    DATE(@endDate) AS end_date,
+    DATE_SUB(DATE(@startDate), INTERVAL 1 DAY) AS prev_end_date,
+    DATE_SUB(DATE(@startDate), INTERVAL DATE_DIFF(DATE(@endDate), DATE(@startDate), DAY) + 1 DAY) AS prev_start_date
+),
 
-  -- Derived sales metrics
-  CASE 
-    WHEN (IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0)) - IFNULL(SUM(ad_revenue), 0) > 0 
-      THEN (IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0)) - IFNULL(SUM(ad_revenue), 0)
-    ELSE 0 
-  END AS organic_sales,
+-- Current period data
+current_period AS (
+  SELECT 
+    IFNULL(SUM(ad_spend), 0) AS ad_spend,
+    IFNULL(SUM(ad_revenue), 0) AS ad_revenue,
+    IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0) AS total_sales,
+    CASE 
+      WHEN (IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0)) - IFNULL(SUM(ad_revenue), 0) > 0 
+        THEN (IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0)) - IFNULL(SUM(ad_revenue), 0)
+      ELSE 0 
+    END AS organic_sales,
+    SAFE_DIVIDE(SUM(ad_spend), SUM(ad_revenue)) AS acos,
+    SAFE_DIVIDE(SUM(ad_revenue), SUM(ad_spend)) AS roas,
+    SAFE_DIVIDE(SUM(ad_spend), (IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0))) AS tacos
+  FROM \`intentwise_ecommerce_graph.account_summary\`, date_ranges
+  WHERE report_date BETWEEN date_ranges.start_date AND date_ranges.end_date
+),
 
-  SAFE_DIVIDE(SUM(ad_revenue), (IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0))) AS ad_sales_attrib,
-  SAFE_DIVIDE(SUM(ad_spend), SUM(ad_revenue)) AS acos,
-  SAFE_DIVIDE(SUM(ad_revenue), SUM(ad_spend)) AS roas,
-  SAFE_DIVIDE(SUM(ad_spend), (IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0))) AS tacos,
+-- Previous period data (auto calculated range)
+previous_period AS (
+  SELECT 
+    IFNULL(SUM(ad_spend), 0) AS ad_spend,
+    IFNULL(SUM(ad_revenue), 0) AS ad_revenue,
+    IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0) AS total_sales,
+    CASE 
+      WHEN (IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0)) - IFNULL(SUM(ad_revenue), 0) > 0 
+        THEN (IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0)) - IFNULL(SUM(ad_revenue), 0)
+      ELSE 0 
+    END AS organic_sales,
+    SAFE_DIVIDE(SUM(ad_spend), SUM(ad_revenue)) AS acos,
+    SAFE_DIVIDE(SUM(ad_revenue), SUM(ad_spend)) AS roas,
+    SAFE_DIVIDE(SUM(ad_spend), (IFNULL(SUM(product_sales), 0) + IFNULL(SUM(ordered_revenue), 0))) AS tacos
+  FROM \`intentwise_ecommerce_graph.account_summary\`, date_ranges
+  WHERE report_date BETWEEN date_ranges.prev_start_date AND date_ranges.prev_end_date
+)
 
-  SAFE_DIVIDE((IFNULL(AVG(product_sales), 0) + IFNULL(AVG(ordered_revenue), 0)), COUNT(DISTINCT report_date)) AS avg_daily_sales,
-  SAFE_DIVIDE((IFNULL(AVG(product_quantity), 0) + IFNULL(AVG(ordered_units), 0)), COUNT(DISTINCT report_date)) AS avg_daily_units
-FROM 
-  \`intentwise_ecommerce_graph.account_summary\`
+-- Final output with percentage changes
+SELECT
+  -- Current period values
+  c.ad_spend,
+  c.ad_revenue,
+  c.total_sales,
+  c.organic_sales,
+  c.acos,
+  c.roas,
+  c.tacos,
 
-WHERE 
-  report_date BETWEEN @startDate AND @endDate;
+  -- % Changes
+  SAFE_DIVIDE((c.ad_spend - p.ad_spend), p.ad_spend) * 100 AS ad_spend_change_pct,
+  SAFE_DIVIDE((c.ad_revenue - p.ad_revenue), p.ad_revenue) * 100 AS ad_revenue_change_pct,
+  SAFE_DIVIDE((c.total_sales - p.total_sales), p.total_sales) * 100 AS total_sales_change_pct,
+  SAFE_DIVIDE((c.organic_sales - p.organic_sales), p.organic_sales) * 100 AS organic_sales_change_pct,
+  SAFE_DIVIDE((c.acos - p.acos), p.acos) * 100 AS acos_change_pct,
+  SAFE_DIVIDE((c.roas - p.roas), p.roas) * 100 AS roas_change_pct,
+  SAFE_DIVIDE((c.tacos - p.tacos), p.tacos) * 100 AS tacos_change_pct
+FROM current_period c, previous_period p;
 `;
 
 const selleCentralMetrices = `
