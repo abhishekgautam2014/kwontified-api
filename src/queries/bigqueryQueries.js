85bd89d9a7e6a521f5d4a70dab670c81bd24c131
abhishekgautam2014 <abhishekgautam76@gmail.com>
2025-11-16 22:39:33 +0530

    matchtype overview query

--- src/queries/bigqueryQueries.js
+++ src/queries/bigqueryQueries.js
@@ -906,6 +906,31 @@ ORDER BY
   brand_type;
 `;
 
+const brandOverview = `
+SELECT 
+  CASE 
+    WHEN is_brand_term = TRUE THEN "Brand" 
+    ELSE "Non Brand" 
+  END AS brand_type,
+  
+  CAST(IFNULL(SUM(ad_spend), 0) AS FLOAT64) AS ad_spend,
+  CAST(IFNULL(SUM(ad_revenue), 0) AS FLOAT64) AS ad_revenue,
+  SAFE_DIVIDE(SUM(ad_revenue), SUM(ad_spend)) AS roas,
+  CAST(SAFE_DIVIDE(SUM(ad_spend), SUM(ad_revenue)) * 100 AS FLOAT64) AS acos,
+FROM 
+  \`intentwise_ecommerce_graph.searchterm_summary\`
+
+WHERE 
+  report_date BETWEEN @startDate AND @endDate
+  AND account_id = @account_id
+  {{where_clause}}
+
+GROUP BY 
+  brand_type
+ORDER BY 
+  brand_type;
+`;
+
 const spendByMatchType = `
 SELECT 
   match_type,
@@ -918,6 +943,8 @@ FROM
 WHERE 
   report_date BETWEEN @startDate AND @endDate
   AND account_id = @account_id
+  AND match_type IS NOT NULL
+  AND match_type != ''
   {{where_clause}}
 
 GROUP BY 
@@ -938,6 +965,8 @@ FROM
 WHERE 
   report_date BETWEEN @startDate AND @endDate
   AND account_id = @account_id
+  AND match_type IS NOT NULL
+  AND match_type != ''
   {{where_clause}}
 
 GROUP BY 
@@ -957,8 +986,31 @@ FROM
 WHERE 
   report_date BETWEEN @startDate AND @endDate
   AND account_id = @account_id
+  AND match_type IS NOT NULL
+  AND match_type != ''
   {{where_clause}}
 
+GROUP BY 
+  match_type
+ORDER BY 
+  match_type;
+`;
+
+const matchTypeOverview = `
+SELECT 
+  match_type,
+  CAST(IFNULL(SUM(ad_spend), 0) AS FLOAT64) AS ad_spend,
+  CAST(IFNULL(SUM(ad_revenue), 0) AS FLOAT64) AS ad_revenue,
+  SAFE_DIVIDE(SUM(ad_revenue), SUM(ad_spend)) AS roas,
+  CAST(SAFE_DIVIDE(SUM(ad_spend), SUM(ad_revenue)) * 100 AS FLOAT64) AS acos
+FROM 
+  \`intentwise_ecommerce_graph.searchterm_summary\`
+WHERE 
+  report_date BETWEEN @startDate AND @endDate
+  AND account_id = @account_id
+  AND match_type IS NOT NULL
+  AND match_type != ''
+  {{where_clause}}
 GROUP BY 
   match_type
 ORDER BY 
@@ -995,7 +1047,9 @@ module.exports = {
 	spendByBrand,
 	revenueByBrand,
 	roasByBrand,
+	brandOverview,
 	spendByMatchType,
 	revenueByMatchType,
 	roasByMatchType,
+	matchTypeOverview,
 };
