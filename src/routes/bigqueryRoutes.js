14e82dc9202b83e651d5bff678e5956e6fb3c604
abhishekgautam2014 <abhishekgautam76@gmail.com>
2025-12-01 21:50:46 +0530

    added middleware in bigquery router

--- src/routes/bigqueryRoutes.js
+++ src/routes/bigqueryRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const bigQueryController = require("../controllers/bigqueryController");
+const authenticateToken = require("../middleware/authMiddleware");
+
+router.use(authenticateToken);
 
 router.get("/test-connection", bigQueryController.testConnection);
 router.get(
